086d2a92fc70dc71698967de901a86251b5e6e03
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-06 15:40:03 +0530

    Security Header Update

--- auth/next.config.mjs
+++ auth/next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+const nextConfig = {
+    reactStrictMode: true,
+
+    // X-DNS-Prefetch-Control
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+
+    // X-Strict-Transport-Security ( Max Age 2 Years )
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+
+    // X-Frame-Options
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+
+    // X-Content-Type-Options
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+
+    // Referrer-Policy
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+};
 
 export default nextConfig;
