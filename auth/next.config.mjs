b04c777fe3b02dd87459657fe5c4a5ea5844e5f2
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-08 13:06:21 +0530

    Security Update

--- auth/next.config.mjs
+++ auth/next.config.mjs
@@ -1,50 +1,39 @@
 /** @type {import('next').NextConfig} */
-
-
-export const nextConfig = {
+const nextConfig = {
     reactStrictMode: true,
 };
 
 const headers = async () => [
     {
-        source: '/(.*)',
+        source: '/(.*)', // Apply headers to all routes
         headers: [
             {
-                reactStrictMode: true,
-            },
-            {
-                // X-DNS-Prefetch-Control
                 key: 'X-DNS-Prefetch-Control',
                 value: 'on',
-            }
-            ,
+            },
             {
-                // X-Strict-Transport-Security ( Max Age 2 Years )
                 key: 'Strict-Transport-Security',
                 value: 'max-age=63072000; includeSubDomains; preload',
             },
             {
-                // X-Frame-Options
                 key: 'X-Frame-Options',
                 value: 'SAMEORIGIN',
             },
             {
-                // X-Content-Type-Options
                 key: 'X-Content-Type-Options',
                 value: 'nosniff',
-
             },
             {
-                // Referrer-Policy
                 key: 'Referrer-Policy',
                 value: 'origin-when-cross-origin',
             },
         ],
     },
-]
-
+];
 
 export default {
     ...nextConfig,
-    headers,
+    async headers() {
+        return headers();
+    },
 };
