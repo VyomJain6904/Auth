d77bbe831119ffed323f8b39a3c2d7996ff7d22c
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-08 13:01:46 +0530

    Security Update 4

--- auth/next.config.mjs
+++ auth/next.config.mjs
@@ -5,45 +5,47 @@ export const nextConfig = {
     reactStrictMode: true,
 };
 
-exports = {
-    ...nextConfig,
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        reactStrictMode: true,
-                    },
-                    {
-                        // X-DNS-Prefetch-Control
-                        key: 'X-DNS-Prefetch-Control',
-                        value: 'on',
-                    }
-                    ,
-                    {
-                        // X-Strict-Transport-Security ( Max Age 2 Years )
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload',
-                    },
-                    {
-                        // X-Frame-Options
-                        key: 'X-Frame-Options',
-                        value: 'SAMEORIGIN',
-                    },
-                    {
-                        // X-Content-Type-Options
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
+const headers = async () => {
+    return [
+        {
+            source: '/(.*)',
+            headers: [
+                {
+                    reactStrictMode: true,
+                },
+                {
+                    // X-DNS-Prefetch-Control
+                    key: 'X-DNS-Prefetch-Control',
+                    value: 'on',
+                }
+                ,
+                {
+                    // X-Strict-Transport-Security ( Max Age 2 Years )
+                    key: 'Strict-Transport-Security',
+                    value: 'max-age=63072000; includeSubDomains; preload',
+                },
+                {
+                    // X-Frame-Options
+                    key: 'X-Frame-Options',
+                    value: 'SAMEORIGIN',
+                },
+                {
+                    // X-Content-Type-Options
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff',
 
-                    },
-                    {
-                        // Referrer-Policy
-                        key: 'Referrer-Policy',
-                        value: 'origin-when-cross-origin',
-                    },
-                ],
-            },
-        ]
-    },
+                },
+                {
+                    // Referrer-Policy
+                    key: 'Referrer-Policy',
+                    value: 'origin-when-cross-origin',
+                },
+            ],
+        },
+    ]
+}
+
+export default {
+    ...nextConfig,
+    headers,
 };
