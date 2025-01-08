727d4115b73ae9d12da8b0140fba59e3644a4631
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-08 12:52:56 +0530

    Security Update

--- auth/next.config.mjs
+++ auth/next.config.mjs
@@ -3,26 +3,49 @@
 
 const nextConfig = {
     reactStrictMode: true,
-
-    // X-DNS-Prefetch-Control
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-
-    // X-Strict-Transport-Security ( Max Age 2 Years )
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-
-    // X-Frame-Options
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-
-    // X-Content-Type-Options
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-
-    // Referrer-Policy
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
 };
+
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        reactStrictMode: true,
+                    },
+                    {
+                        // X-DNS-Prefetch-Control
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on',
+                    }
+                    ,
+                    {
+                        // X-Strict-Transport-Security ( Max Age 2 Years )
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+
+                    },
+                    {
+                        // X-Frame-Options
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        // X-Content-Type-Options
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+
+                    },
+                    {
+                        // Referrer-Policy
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                ],
+            },
+        ]
+    },
+}
 
 export default nextConfig;
