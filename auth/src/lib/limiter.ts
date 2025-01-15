09ec2c080b6dad553c7f34218548d1fff6ccd52e
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-15 16:07:49 +0530

    Limiter Fnc

---
+++ auth/src/lib/limiter.ts
@@ -0,0 +1,49 @@
+import { getIp } from "@/lib/get-ip";
+
+const trackers: Record<string, { count: number; expiresAt: number }> = {};
+
+export async function rateLimitByIp ( limit : number = 1 , interval : number = 1000000 ) {
+    const ip = getIp();
+
+    if ( !ip ) {
+        throw new Error ( "IP Address not found" );
+    }
+
+    const tracker = trackers[ip] || { count: 0, expiresAt: 0 };
+
+    if ( !trackers[ip] ) {
+        trackers[ip] = tracker;
+    }
+
+    if ( tracker.expiresAt < Date.now() ) {
+        tracker.count = 0;
+        tracker.expiresAt = Date.now() + interval;
+    }
+
+    tracker.count++;
+
+    if ( tracker.count > limit ) {
+        throw new Error ( "Rate limit exceeded" );
+    }
+
+}
+
+export async function rateLimitByKey ( key : string , limit : number = 1 , interval : number = 1000000 ) {
+    const tracker = trackers[key] || { count: 0, expiresAt: 0 };
+
+    if ( !trackers[key] ) {
+        trackers[key] = tracker;
+    }
+
+    if ( tracker.expiresAt < Date.now() ) {
+        tracker.count = 0;
+        tracker.expiresAt = Date.now() + interval;
+    }
+
+    tracker.count++;
+
+    if ( tracker.count > limit ) {
+        throw new Error ( "Rate limit exceeded" );
+    }
+
+}
