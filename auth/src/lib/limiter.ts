e29b1a2f008ca04959c6baac755f811504c22d29
VyomOp2 <vyom.malo6904@gmail.com>
2025-01-15 18:05:37 +0530

    Rate Limiter Setup

--- auth/src/lib/limiter.ts
+++ auth/src/lib/limiter.ts
@@ -9,22 +9,7 @@ export async function rateLimitByIp ( limit : number = 1 , interval : number = 1
         throw new Error ( "IP Address not found" );
     }
 
-    const tracker = trackers[ip] || { count: 0, expiresAt: 0 };
-
-    if ( !trackers[ip] ) {
-        trackers[ip] = tracker;
-    }
-
-    if ( tracker.expiresAt < Date.now() ) {
-        tracker.count = 0;
-        tracker.expiresAt = Date.now() + interval;
-    }
-
-    tracker.count++;
-
-    if ( tracker.count > limit ) {
-        throw new Error ( "Rate limit exceeded" );
-    }
+    rateLimitByKey(ip, limit, interval);
 
 }
 
